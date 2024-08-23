f426242383a0239dde7bdf11898a0ebaa38b5cbd
Aditya Suryawanshi <adityasuryawanshi786@gmail.com>
2024-08-23 18:00:24 +0530

    v1

---
+++ adi/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+import pickle
+
+app = Flask(__name__)
+
+# @app.route("/")
+# def hello_world():
+#     return "<p>Hello, World!</p>"
+
+@app.route("/ping", methods = ["GET"] )
+def ping():
+    return {"message" : "Hi there, I am working!!"}
